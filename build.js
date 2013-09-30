bb94084352141b68d55be5faf080800a4ad89dba
okunishinishi <okunishinishi@gmail.com>
2013-09-30 13:57:13 +0900

    add demo

--- build.js
+++ build.js
@@ -2,12 +2,16 @@
 
 var tek = require('tek'),
     fs = require('fs'),
+    Format = tek['Format'],
     file = tek['file'],
     isDir = file['isDir'],
+    EOL = require('os')['EOL'],
+    TAB = '\t',
     resolve = require('path')['resolve'],
     JobQueue = tek['JobQueue'];
 
 var distDir = resolve(__dirname, 'dist'),
+    apartDistDir = resolve(distDir, 'apart'),
     libDir = resolve(__dirname, 'lib');
 
 
@@ -15,6 +19,11 @@ function handleErr(err) {
     err && console.error(err);
     return !!err;
 }
+
+function indent(string) {
+    return TAB + string.replace(new RegExp(EOL, 'mg'), EOL + TAB);
+}
+
 
 var jobQueue = new JobQueue;
 
@@ -53,9 +62,20 @@ jobQueue
         new JobQueue().pushAll(jobs).execute(next);
     })
     .push(function (next) {
+        fs.exists(apartDistDir, function (exits) {
+            if (exits) {
+                next();
+            } else {
+                fs.mkdir(apartDistDir, function (err) {
+                    err && handleErr(err);
+                    next();
+                });
+            }
+        });
+    })
+    .push(function (next) {
         var copy = file['copy'],
-            getExtension = file['getExtension'],
-            EOL = require('os')['EOL'];
+            getExtension = file['getExtension'];
 
         fs.readdir(libDir, function (err, filenames) {
             err && handleErr(err);
@@ -73,7 +93,7 @@ jobQueue
                     if (isDir(filepath)) {
                         return function (next) {
                             allContentsInDir(filepath, function (contents) {
-                                fs.writeFile(resolve(distDir, filename), contents.join(EOL), function (err) {
+                                fs.writeFile(resolve(apartDistDir, filename), contents.join(EOL), function (err) {
                                     err && handleErr(err);
                                     next();
                                 });
@@ -81,12 +101,72 @@ jobQueue
                         }
                     } else {
                         return function (next) {
-                            copy(filepath, resolve(distDir, filename), next);
+                            var isFormatFile = filepath.match(/\.format$/);
+                            if (isFormatFile) {
+                                next();
+                                return;
+                            }
+                            copy(filepath, resolve(apartDistDir, filename), next);
                         };
                     }
                 });
             new JobQueue().pushAll(jobs).execute(next);
         });
+    })
+    .push(function (next) {
+        var getExtension = file['getExtension'];
+        var formatFile = resolve(libDir, 'para.js.format'),
+            format = null,
+            data = {};
+        new JobQueue()
+            .push(function (next) {
+                fs.readFile(formatFile, function (err, buffer) {
+                    err && handleErr(err);
+                    format = new Format(buffer.toString());
+                    (err ? next.abort : next)();
+                });
+            })
+            .pushAll(
+
+                fs.readdirSync(apartDistDir).map(function (filename) {
+                    return function (next) {
+                        var filepath = resolve(apartDistDir, filename);
+                        fs.readFile(filepath, function (err, buffer) {
+                            err && handleErr();
+
+                            var content = buffer.toString();
+                            switch (getExtension(filename)) {
+                                case ".js":
+                                    content = indent(content);
+                                    break;
+                                case ".css":
+                                    content = content.replace(new RegExp(EOL, "g"), '');
+                                    break;
+                            }
+
+                            data[filename] = indent(content);
+
+                            (err ? next.abort : next)();
+                        });
+                    };
+                })
+            ).execute(function () {
+                var UglifyJS;
+                UglifyJS = require("uglify-js");
+                var outpath = resolve(distDir, 'para.js'),
+                    minOutpath = outpath.replace(/\.js$/, ".min.js");
+                var content = format.apply(data);
+                fs.writeFile(outpath, content, function (err) {
+                    err && handleErr(err);
+                    console.log('new file published:', outpath);
+                    var mini = UglifyJS.minify(content, {fromString: true});
+                    fs.writeFile(minOutpath, mini.code, function (err) {
+                        err && handleErr(err);
+                        console.log('new file published:', minOutpath);
+                        (err ? next.abort : next)();
+                    });
+                });
+            });
     })
     .execute(function () {
         console.log('[para.js] build done!');
